7e1c4cee423b88045a374468fdd1ada835b12283
Jacco van Dalfsen <javdalfsen@student.che.nl>
2024-02-12 11:38:25 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+# Opdracht 1.1 - Hello World
+# Print "Hello World!" op de console
+print("Hello World!")
+
+# Opdracht 1.2 - Functie zonder parameter
+def goedenavond():
+    print("Goedenavond")
+
+# Opdracht 1.3 - Functie met parameter
+# Zorg ervoor dat de naam op de console wordt geprint.
+def groet(naam):
+    print(f"Hallo, {naam}!")
+
+# Opdracht 2.1 - Functie om een keersom te maken
+def keer(a: int, b: int):
+    antwoord = a * b
+    print(f"Het antwoord op deze keersom is: {antwoord}")
+
+# Main functie 
+def main():
+    goedenavond()
+
+    naam = input("Voer je naam in: ")
+    groet(naam)
+
+    cijfer1 = int(input("Voer het eerste cijfer voor de keersom in: "))
+    cijfer2 = int(input("Voer het tweede cijfer in: "))
+    keer(cijfer1, cijfer2)
+
+# Dit zorgt ervoor dat de main functie wordt aangeroepen.
+# Hier hoef je niet aan te zitten
+if __name__ == "__main__":
+    main()
